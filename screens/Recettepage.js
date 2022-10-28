fc3726b8dd4f630255fe761dbb7ce48e9d4f05a5
Celine <celine.sh.tan@gmail.com>
2022-10-28 11:39:37 +0200

    recettepage incoming change

--- screens/Recettepage.js
+++ screens/Recettepage.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import { useState, useEffect } from "react";
->>>>>>> 29c65feaba698d0a7b3cbfde84755e8e96ad2ccf
 import {
   StyleSheet,
   Platform,
@@ -13,30 +10,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React from "react";
-<<<<<<< HEAD
-import Ionicons from "react-native-vector-icons/Ionicons";
-import Homepage from "./Homepage";
-import { useNavigation } from "@react-navigation/native";
-
-export default function Recettepage(navigation) {
-  let ideeRecette = [
-    {
-      id: 4,
-      Name: "Steak with Oriental vegetables",
-      desc: "An old favorite gets and exotic touch",
-      image: require("../assets/achat.png"),
-      color: "#FD9B9E",
-      serving: "1 plate (500 - 600G)",
-      servingNb: 1,
-      longDesc:
-        "Mouth-melting carrots and tender turnips roasted in Moroccan spices accompany a classic, juicy steak.",
-      level: "easy",
-      time: "35 min",
-      rating: 4.2,
-    },
-  ];
-=======
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Homepage from "./Homepage";
 
 export default function Recettepage(navigation) {
   const [listRecipe, setListRecipe] = useState([]);
@@ -82,7 +57,6 @@ export default function Recettepage(navigation) {
         setListRecipe(data.recipes);
       });
   }
->>>>>>> 29c65feaba698d0a7b3cbfde84755e8e96ad2ccf
 
   function handlePressDessert() {
     fetch(
@@ -128,63 +102,6 @@ export default function Recettepage(navigation) {
   });
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <TouchableOpacity
-        onPress={() => navigation.navigate(Recettepage)}
-        style={styles.button1}
-        activeOpacity={0.8}
-      ></TouchableOpacity>
-      <Button title="retour" onPress={() => navigation.navigate("Homepage")} />
-      <Button onPress={Homepage} title="Retour" color="#841584" />
-      <Text style={styles.title}>Choisis ta recette</Text>
-
-      <View style={styles.cards}>
-        <View style={styles.carteRecette}>
-          <Ionicons
-            name="calendar"
-            size={24}
-            color="#dedede"
-            style={styles.iconCalendar}
-          />
-          <Ionicons
-            name="time"
-            size={24}
-            color="#dedede"
-            style={styles.iconTime}
-          />
-          <Image
-            style={styles.imageRecette1}
-            source={require("../assets/plat.png")}
-          />
-          <Ionicons name="heart" size={24} style={styles.iconContent} />
-        </View>
-        <View></View>
-        <View style={styles.carteRecette}>
-          <Ionicons
-            name="calendar"
-            size={24}
-            color="#dedede"
-            style={styles.iconCalendar}
-          />
-          <Ionicons
-            name="time"
-            size={24}
-            color="#dedede"
-            style={styles.iconTime}
-          />
-          <Image
-            style={styles.imageRecette1}
-            source={require("../assets/pizza.png")}
-          />
-          <Ionicons
-            name="heart"
-            size={24}
-            color="#dedede"
-            style={styles.iconContent}
-          />
-        </View>
-      </View>
-=======
       <View style={styles.containerHeader}>
         <View>
           <Text style={styles.welcomeText}>Hello Phifi</Text>
@@ -246,7 +163,6 @@ export default function Recettepage(navigation) {
       <ScrollView>
         <View style={styles.containerRecipes}>{Recipes}</View>
       </ScrollView>
->>>>>>> 29c65feaba698d0a7b3cbfde84755e8e96ad2ccf
     </View>
   );
 }
@@ -255,69 +171,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-<<<<<<< HEAD
-    paddingTop: 110,
-  },
-
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#655074",
-    marginLeft: 20,
-    fontFamily: Platform.select({ ios: "Georgia", android: "serif" }),
-    marginBottom: 20,
-  },
-  cards: {
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    padding: 10,
-    alignContent: "space-between",
-  },
-  button1: {
-    borderWidth: 1,
-    borderColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 100,
-    height: 100,
-    backgroundColor: "#fff",
-    borderRadius: 100,
-    position: "absolute",
-    top: 300,
-  },
-  carteRecette: {
-    backgroundColor: "#F9D77E",
-    width: 200,
-    height: 250,
-    borderBottomLeftRadius: 40,
-    borderBottomRightRadius: 40,
-  },
-
-  imageRecette1: {
-    justifyContent: "center",
-
-    width: 180,
-    height: 180,
-  },
-  iconContent: {
-    position: "absolute",
-    top: 10,
-    right: 10,
-    color: "red",
-  },
-  iconCalendar: {
-    position: "absolute",
-    bottom: 30,
-    right: 10,
-    color: "black",
-  },
-  iconTime: {
-    position: "absolute",
-    bottom: 30,
-    left: 10,
-    color: "black",
-=======
     paddingTop: 45,
     paddingHorizontal: 20,
   },
@@ -448,6 +301,5 @@ const styles = StyleSheet.create({
     top: 10,
     right: 10,
     color: "red",
->>>>>>> 29c65feaba698d0a7b3cbfde84755e8e96ad2ccf
   },
 });
