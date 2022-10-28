d87dedef33a827ed73ba58e781f8be8596edf3aa
Mariem <rjeibi.maryem2@gmail.com>
2022-10-28 10:00:25 +0200

    recettePage

--- App.js
+++ App.js
@@ -54,11 +54,11 @@ export default function App() {
           <Stack.Navigator screenOptions={{ headerShown: false }}>
             {/* <Stack.Screen name="ConceptScreen" component={ConceptScreen} />
             <Stack.Screen name="FormScreen" component={FormScreen} />
-            <Stack.Screen name="SignupScreen" component={SignupScreen} />
-            <Stack.Screen name="Homepage" component={Homepage} /> */}
+            <Stack.Screen name="SignupScreen" component={SignupScreen} />*/}
+            <Stack.Screen name="Homepage" component={Homepage} />
             <Stack.Screen name="Recettepage" component={Recettepage} />
-            {/* <Stack.Screen name="Placard" component={Placard} /> */}
-            {/* <Stack.Screen name="Filter" component={Filter} /> */}
+            {/* <Stack.Screen name="Placard" component={Placard} />
+            <Stack.Screen name="Filter" component={Filter} /> */}
           </Stack.Navigator>
         </NavigationContainer>
       </PersistGate>
